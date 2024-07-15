12280ddb547c30c3e6cfcfb8ddbb028406f47e36
PG-God <t.fukuoka.23.6.1075@4nonome.com>
2024-07-15 18:24:30 +0900

    test

--- Assets/test_1.cs
+++ Assets/test_1.cs
@@ -1,16 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class test_1 : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
+        //aiueo
     }
 
-    // Update is called once per frame
     void Update()
     {
         

f7e741eaf3ed63d70459781c69beb5d785179a0c
PG-God <t.fukuoka.23.6.1075@4nonome.com>
2024-10-18 20:37:23 +0900

    coinの実装修正

---
+++ Assets/Scripts/CoinManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinManager : MonoBehaviour
+{
+    [SerializeField] private Text coinText;
+    private int score = 0;
+
+    private void Start()
+    {
+        UpdateCoinText();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("coin"))
+        {
+            score++;
+            UpdateCoinText();
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void UpdateCoinText()
+    {
+        coinText.text = $"{score} コイン";
+    }
+}

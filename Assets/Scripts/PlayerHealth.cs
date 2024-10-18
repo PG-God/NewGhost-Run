d921641b94e7ceecb7b9c6e910108e39e8565c15
PG-God <t.fukuoka.23.6.1075@4nonome.com>
2024-10-18 17:57:07 +0900

    HP機能の追加

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 3;
+    private int currentHealth;
+
+    public GameObject heart1;
+    public GameObject heart2;
+    public GameObject heart3;
+
+    public Text gameOverText;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        gameOverText.gameObject.SetActive(false);
+        UpdateHeartsUI();
+    }
+
+    void UpdateHeartsUI()
+    {
+        heart1.SetActive(currentHealth >= 1);
+        heart2.SetActive(currentHealth >= 2);
+        heart3.SetActive(currentHealth >= 3);
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        UpdateHeartsUI();
+
+        if (currentHealth <= 0)
+        {
+            GameOver();
+        }
+    }
+
+    void GameOver()
+    {
+        SceneManager.LoadScene("ResultScenes");
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            TakeDamage(1);
+        }
+    }
+}
